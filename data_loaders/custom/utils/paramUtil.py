4c7f6d0d5501b4a4b47e1a775d9714715402c1d3
icedwater <icedwater@gmail.com>
2024-10-25 09:07:50 +0800

    Updated custom paramUtil with sample skeleton.

--- data_loaders/custom/utils/paramUtil.py
+++ data_loaders/custom/utils/paramUtil.py
@@ -1,34 +1,15 @@
+"""
+Parameters for the reference skeleton.
+
+Each skeleton must have:
+- kinematic chain: list of lists that reflect joint hierarchy
+- raw offsets: np.array of relative positions to parent node in [x, y, z] order
+- tgt_skel_id: serial number of the file to read the example skeleton from
+"""
+
 import numpy as np
 
 # Define a kinematic tree for the skeletal struture
-kit_kinematic_chain = [[0, 11, 12, 13, 14, 15], [0, 16, 17, 18, 19, 20], [0, 1, 2, 3, 4], [3, 5, 6, 7], [3, 8, 9, 10]]
-
-kit_raw_offsets = np.array(
-    [
-        [0, 0, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 0, 0],
-        [0, -1, 0],
-        [0, -1, 0],
-        [-1, 0, 0],
-        [0, -1, 0],
-        [0, -1, 0],
-        [1, 0, 0],
-        [0, -1, 0],
-        [0, -1, 0],
-        [0, 0, 1],
-        [0, 0, 1],
-        [-1, 0, 0],
-        [0, -1, 0],
-        [0, -1, 0],
-        [0, 0, 1],
-        [0, 0, 1]
-    ]
-)
-
 t2m_raw_offsets = np.array([[0,0,0],
                            [1,0,0],
                            [-1,0,0],
@@ -53,11 +34,38 @@ t2m_raw_offsets = np.array([[0,0,0],
                            [0,-1,0]])
 
 t2m_kinematic_chain = [[0, 2, 5, 8, 11], [0, 1, 4, 7, 10], [0, 3, 6, 9, 12, 15], [9, 14, 17, 19, 21], [9, 13, 16, 18, 20]]
-t2m_left_hand_chain = [[20, 22, 23, 24], [20, 34, 35, 36], [20, 25, 26, 27], [20, 31, 32, 33], [20, 28, 29, 30]]
-t2m_right_hand_chain = [[21, 43, 44, 45], [21, 46, 47, 48], [21, 40, 41, 42], [21, 37, 38, 39], [21, 49, 50, 51]]
-
-
-kit_tgt_skel_id = '03950'
 
 t2m_tgt_skel_id = '000021'
 
+custom_kinematic_chain = [[0, 1, 2, 3, 4, 5, 6], [1, 7, 8, 9, 10, 11], [1, 12, 13, 14, 15, 16], [13, 17, 18, 19, 20, 21], [13, 22, 23, 24, 25, 26]]
+custom_raw_offset = np.array(
+    [
+        [ 0, 0, 0],
+        [ 0, 1, 0],
+        [ 1, 0, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [-1, 0, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0, 1, 0],
+        [ 0, 1, 0],
+        [ 0, 1, 0],
+        [ 0, 1, 0],
+        [ 0, 1, 0],
+        [-1, 0, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 1, 0, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0],
+        [ 0,-1, 0]
+    ]
+)
